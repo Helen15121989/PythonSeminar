3439a020ae886d863c01f0bd325b8135d2b7866d
Helen <>
2023-03-18 22:04:41 +0300

    seminar7

--- Seminar7/task7.py
+++ Seminar7/task7.py
@@ -51,10 +51,10 @@
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
-        answer = []
+        result = []
         for j in range(1, num_columns + 1):
-            answer.append(str(operation(i, j)))
-        print("\t".join(answer))
+            result.append(str(operation(i, j)))
+        print("\t".join(result))
 
 
 print_operation_table(lambda x, y: x * y)
